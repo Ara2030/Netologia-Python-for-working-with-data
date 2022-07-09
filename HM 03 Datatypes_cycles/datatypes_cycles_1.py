6995e6c8f225b811eb29b85fc0ee39a4ca3edf84
Araik Margaryan <maraik678@yandex.com>
2022-07-09 20:30:01 +0300

    Perfect couples

--- HM 03 Datatypes_cycles/datatypes_cycles_1.py
+++ HM 03 Datatypes_cycles/datatypes_cycles_1.py
@@ -4,6 +4,9 @@
 # - среднюю букву, если число букв в слове нечетное;
 # - две средних буквы, если число букв четное.
 
+from ntpath import join
+
+
 word = "testing"
 if len(word) % 2 == 0:
     print (word [1:3])
@@ -20,3 +23,22 @@ while True:
         break
     else: numbers += numbers_1
 print ("Сумма: ", numbers)
+
+## Задание 3
+# Мы делаем [MVP] dating-сервиса, и у нас есть список парней и девушек.  
+# Выдвигаем гипотезу: лучшие рекомендации мы получим, если просто отсортируем имена по алфавиту и 
+# познакомим людей с одинаковыми индексами после сортировки!
+# Но мы не будем никого знакомить, если кто-то может остаться без пары:
+print()
+boys = ['Peter', 'Alex', 'John', 'Arthur', 'Richard']
+girls = ['Kate', 'Liza', 'Kira', 'Emma', 'Tanya']
+boys.sort()
+girls.sort()
+perfect_couples = zip(boys, girls)
+print ("Идеальная пара")
+for couple in (list(perfect_couples)):
+    print(",".join(couple))
+    if len(girls) == len(boys):
+        pass
+    else:
+        print ("Внимание, кто-то может остаться без пары!")
